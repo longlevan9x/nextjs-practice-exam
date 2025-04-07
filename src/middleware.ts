98a2f55ccefbc2551c4737824d57229f2bb11cb7
Long Van <longvan121996@gmail.com>
2025-04-07 23:20:38 +0900

    Add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  // Chuyển hướng từ /exams về /
+  if (request.nextUrl.pathname === '/exams') {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/exams'],
+}; 
