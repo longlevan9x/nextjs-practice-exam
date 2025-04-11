937da74f5f51f60d641fb9296d62b5a2cc5d37f2
Long Van <longvan121996@gmail.com>
2025-04-11 17:15:25 +0900

    Update linter

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,10 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  console.log(pathname);
+  
   return NextResponse.next();
 }
 
