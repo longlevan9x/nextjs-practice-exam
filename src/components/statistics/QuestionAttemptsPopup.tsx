d47c864e5d20a40ca71f1b7d826e18780ef48be4
Long Van <longvan121996@gmail.com>
2025-05-09 15:03:07 +0900

    Update Most Incorrect Question

--- src/components/statistics/QuestionAttemptsPopup.tsx
+++ src/components/statistics/QuestionAttemptsPopup.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { ExamResult } from '@/types/ExamResult';
-import { Question } from '@/types/question';
+import { Answer, Question } from '@/types/question';
 import AnswerExplanation from '@/components/examDetail/AnswerExplanation';
 import References from '@/components/examDetail/References';
 import { EXPLANATION_SECTION_TITLE } from '@/constants/constants';
@@ -17,21 +17,40 @@ const QuestionAttemptsPopup: React.FC<QuestionAttemptsPopupProps> = ({
   examResults,
   onClose,
 }) => {
+
   // Lọc các lần làm bài có câu hỏi này
   const attempts = examResults
-    .filter(result => 
+    .filter(result =>
       result.questions.some(q => q.id === question.id && q.selectedAnswer !== null)
     )
     .map(result => {
       const questionResult = result.questions.find(q => q.id === question.id);
+      const questionAnswers = question?.answers?.map(answer => {
+        if (!questionResult?.selectedAnswer) {
+          return null;
+        }
+
+        if (Array.isArray(questionResult.selectedAnswer) && !questionResult.selectedAnswer.includes(answer.id)) {
+          return null;
+        }
+
+        if (!Array.isArray(questionResult.selectedAnswer) && questionResult.selectedAnswer !== answer.id) {
+          return null;
+        }
+
+        return answer;
+      }).filter(answer => answer !== null) as Answer[];
+
       return {
         date: new Date(result.endTime || ''),
         selectedAnswer: questionResult?.selectedAnswer,
         isCorrect: questionResult?.isCorrect,
         examId: result.examId,
+        answers: questionAnswers || [],
       };
     })
     .sort((a, b) => b.date.getTime() - a.date.getTime());
+
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className=" bg-black opacity-90 absolute w-full h-full z-10"></div>
@@ -58,9 +77,8 @@ const QuestionAttemptsPopup: React.FC<QuestionAttemptsPopupProps> = ({
           {attempts.map((attempt, index) => (
             <div
               key={index}
-              className={`border rounded-lg p-3 ${
-                attempt.isCorrect ? 'bg-green-50' : 'bg-red-50'
-              }`}
+              className={`border rounded-lg p-3 ${attempt.isCorrect ? 'bg-green-50' : 'bg-red-50'
+                }`}
             >
               <div className="flex justify-between items-center">
                 <div>
@@ -72,19 +90,22 @@ const QuestionAttemptsPopup: React.FC<QuestionAttemptsPopupProps> = ({
                   </span>
                 </div>
                 <span
-                  className={`text-sm font-medium ${
-                    attempt.isCorrect ? 'text-green-600' : 'text-red-600'
-                  }`}
+                  className={`text-sm font-medium ${attempt.isCorrect ? 'text-green-600' : 'text-red-600'
+                    }`}
                 >
                   {attempt.isCorrect ? 'Đúng' : 'Sai'}
                 </span>
               </div>
-              {!attempt.isCorrect && attempt.selectedAnswer !== null && (
-                <div className="mt-2">
+              {attempt.selectedAnswer !== null && (
+                <div className="mt-2 flex flex-col space-y-2">
                   <span className="text-sm text-gray-600">Đáp án đã chọn: </span>
-                  <span className="text-sm font-medium text-red-600">
-                    {question.answers.find(a => a.id === attempt.selectedAnswer)?.answer}
-                  </span>
+                  {
+                    attempt.answers.map(answer => (
+                      <span key={attempt.examId + answer.id} className={`text-sm font-medium ${!answer.correct ? 'text-red-600' : ''}`} >
+                        {answer.id}. {answer.answer}
+                      </span>
+                    ))
+                  }
                 </div>
               )}
             </div>
@@ -92,28 +113,28 @@ const QuestionAttemptsPopup: React.FC<QuestionAttemptsPopupProps> = ({
         </div>
 
         <div className={`mt-6 p-4 bg-gray-50 border border-gray-300 rounded-xs transition-all duration-300 delay-100`}>
-            <h3 className="text-lg font-semibold text-gray-800 mb-4">{EXPLANATION_SECTION_TITLE}</h3>
+          <h3 className="text-lg font-semibold text-gray-800 mb-4">{EXPLANATION_SECTION_TITLE}</h3>
 
-            {question.correctAnswerExplanations && (
-              <AnswerExplanation
-                answerExplanations={question.correctAnswerExplanations}
-                type="correct"
-              />
-            )}
+          {question.correctAnswerExplanations && (
+            <AnswerExplanation
+              answerExplanations={question.correctAnswerExplanations}
+              type="correct"
+            />
+          )}
 
-            {question.incorrectAnswerExplanations && question.incorrectAnswerExplanations.length > 0 && (
-              <AnswerExplanation
-                answerExplanations={question.incorrectAnswerExplanations}
-                type="incorrect"
-              />
-            )}
+          {question.incorrectAnswerExplanations && question.incorrectAnswerExplanations.length > 0 && (
+            <AnswerExplanation
+              answerExplanations={question.incorrectAnswerExplanations}
+              type="incorrect"
+            />
+          )}
 
-            {question.references && question.references.length > 0 && (
-              <References references={question.references} />
-            )}
-          </div>
+          {question.references && question.references.length > 0 && (
+            <References references={question.references} />
+          )}
+        </div>
       </div>
-    </div>
+    </div >
   );
 };
 
