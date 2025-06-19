bb0ff4a095a751caba4a57ecfe43b1d717a46407
Long Van <longvan121996@gmail.com>
2025-06-19 23:43:04 +0900

    Update promp

--- src/components/askAI/AskAIChatGPTModal.tsx
+++ src/components/askAI/AskAIChatGPTModal.tsx
@@ -61,10 +61,22 @@ const AskAIChatGPTModal: React.FC<AskChatGPTModalProps> = ({ toolType, content =
         let prompt = "";
 
         if (toolType === "tran") {
-            prompt += "Hãy đoạn văn bản bên dưới sang tiếng việt cho tôi. Yêu cầu: \n1. Giữ lại các keywords. \n 2. Những đoạn về code hãy trả về để hiển thị định dạng code được.  \n";
+            prompt += "Tôi muốn bạn đóng vai trò là chuyên gia ngôn ngữ Anh - Việt. Hãy giúp tôi dịch đoạn tiếng Anh sau.\n";
+            prompt += "Yêu cầu:\n";
+            prompt += "1. Dịch sang tiếng Việt tự nhiên, sát nghĩa, nhưng vẫn giữ đúng sắc thái gốc.\n";
+            prompt += "2. Giữ nguyên các từ khóa tiếng Anh nếu mang nghĩa chuyên ngành.\n";
+            prompt += "Văn bản cần dịch:\n";
+            // prompt += "Hãy đoạn văn bản bên dưới sang tiếng việt cho tôi. Yêu cầu: \n1. Giữ lại các keywords. \n 2. Những đoạn về code hãy trả về để hiển thị định dạng code được.  \n";
         }
         else if (toolType === "explain") {
-            prompt += "Hãy phân tích kỹ. Sử dụng dữ liệu mới nhất. Đầu tiên đưa ra đáp án đúng. Sau đó giải thích các đáp án cho tôi. Giải thích bằng tiếng việt. \n";
+            prompt += "Hãy phân tích kỹ câu hỏi trắc nghiệm sau đây. Chủ đề liên quan đến AWS.\n";
+            prompt += "Yêu cầu:\n";
+            prompt += "1. Đưa ra đáp án đúng trước, chỉ rõ lý do chọn.\n";
+            prompt += "2. Giải thích tất cả các phương án (đúng và sai), nêu rõ vì sao đúng, vì sao sai, nếu có ví dụ minh họa thì càng tốt.\n";
+            prompt += "3. Sử dụng kiến thức cập nhật mới nhất đến năm 2025 (vui lòng áp dụng theo phiên bản mới nhất).\n";
+            prompt += "4. Trình bày hoàn toàn bằng tiếng Việt, rõ ràng, dễ hiểu.\n";
+            prompt += "Văn bản cần giải thích:\n";
+            // prompt += "Hãy phân tích kỹ. Sử dụng dữ liệu mới nhất. Đầu tiên đưa ra đáp án đúng. Sau đó giải thích các đáp án cho tôi. Giải thích bằng tiếng việt. \n";
         }
 
         prompt += content;
@@ -87,7 +99,7 @@ const AskAIChatGPTModal: React.FC<AskChatGPTModalProps> = ({ toolType, content =
             </div>
 
             <div className="w-full border-b border-b-gray-200  dark:border-b-gray-600"></div>
-            <div className={`ai-content-wrapper overflow-auto max-h-[calc(100vh-200px)]  border-gray-200 dark:border-gray-700 rounded-xs bg-white dark:bg-gray-900 text-sm flex-grow`}>
+            <div className={`ai-content-wrapper overflow-auto h-[calc(100vh-16rem)]  border-gray-200 dark:border-gray-700 rounded-xs bg-white dark:bg-gray-900 text-sm flex-grow`}>
                 <ReactMarkdown>{streamText}</ReactMarkdown>
             </div>
 
